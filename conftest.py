bb2aea76a8d467c6e34092f9ffce6935e0d9843c
Anna <verdyanna@gmail.com>
2016-05-08 22:51:34 +0300

    2.4 - запуск 3 тестов в одном браузере - scope -ok

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fixture.application import Application
 
-@pytest.fixture
+@pytest.fixture (scope = "session")
 def app(request):
     fixture = Application() #инициализация фикстуры
     request.addfinalizer(fixture.destroy)  #указание на то, как должна быть разрушена фикстура
